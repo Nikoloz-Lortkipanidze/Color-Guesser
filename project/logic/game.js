41d3b0e6f257eca7f0de670fe94d6245ec650071
Nikoloz Lorkipanidze <nikusha191208@gmail.com>
2025-01-20 22:11:20 +0400

    setting guess color section background color

--- project/logic/game.js
+++ project/logic/game.js
@@ -1,11 +1,19 @@
+let Section_GuessColor = document.querySelector('.Section_GuessTheColor')
 let Section_Circles = document.querySelector('.Section_ColorCircles')
 
 let circlesCount = 4;
+let colors;
+let color;
+
+function startGame(){
+    generateCircles()
+    setBackgroundColor()
+}
 
 function generateCircles(){
     /* Generate & Store Colors */
-    let colors = generateRandomColors(4)
-    let color = getRandomElement(colors)
+    colors = generateRandomColors(4)
+    color = getRandomElement(colors)
 
     /* Create Circles */
     for (let i = 0; i < circlesCount; i++){
@@ -16,4 +24,8 @@ function generateCircles(){
     }
 }
 
-generateCircles()
+function setBackgroundColor(){
+    Section_GuessColor.style.backgroundColor = color
+}
+
+startGame()
